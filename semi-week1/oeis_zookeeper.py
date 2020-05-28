38628bde441eb6cd394245025eb52760f5fc45b4
SammieO52 <65922723+SammieO52@users.noreply.github.com>
2020-05-27 21:39:06 -0400

    Create oeis_zookeeper.py

---
+++ semi-week1/oeis_zookeeper.py
@@ -0,0 +1,10 @@
+# I'm am in part 3/4
+# here is one of the questions I solved
+
+# Write a program that constructs a list from the given string's symbols and then prints it.
+#The variable input_str has been defined for you. So, if input_str, for example, 
+#contains a string "python", your program should turn it to the list ['p', 'y', 't', 'h', 'o', 'n'] and print it.
+
+print(list(input_str))  # use the predefined variable "input_str" here
+
+
